61b151344d945ec304cf48edfa44bc29b3bfd081
wangkaixun <904028997@qq.com>
2020-06-29 18:31:37 +0800

    angularDart Demo 备份

---
+++ lib/app_component.dart
@@ -0,0 +1,24 @@
+import 'package:angular/angular.dart';
+
+import 'src/hero.dart';
+import 'src/hero_component.dart';
+import 'src/mock_heroes.dart';
+
+@Component(
+  selector: 'my-app',
+  templateUrl: 'app_component.html',
+  styleUrls: ['app_component.css'],
+  directives: [coreDirectives, HeroComponent],
+)
+class AppComponent {
+  final title = 'Tour of Heroes';
+  List<Hero> heroes = mockHeroes;
+  Hero selected;
+
+  void onSelect(Hero hero) => selected = onMySelect(hero);
+}
+
+Hero onMySelect(Hero hero) {
+  print(hero.name);
+  return hero;
+}
---
+++ lib/src/hero_component.dart
@@ -0,0 +1,26 @@
+import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+
+import 'hero.dart';
+
+@Component(
+  selector: 'my-hero',
+  styleUrls: ['hero.css'],
+  template: '''
+    <div *ngIf="hero != null">
+      <h2>{{hero.name}}</h2>
+      <div class="fontNormal" style="color: green">
+      <label class="fontNormal" style="margin-bottom: 8px;color: #039be5">id: </label>
+      {{hero.id}}
+      </div>
+      <div style="margin-top: 16px">
+        <label class="fontNormal" style="margin-bottom: 8px;color: #039be5">name: </label>
+        <input class="fontNormal inputCustom" [(ngModel)]="hero.name" placeholder="name" >
+      </div>
+    </div>''',
+  directives: [coreDirectives, formDirectives],
+)
+class HeroComponent {
+  @Input()
+  Hero hero;
+}
